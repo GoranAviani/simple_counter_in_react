ffb38a9cd9c11f3897649a4a1d11028a91ff2492
GoranAviani <goranaviani@gmail.com>
2022-05-13 20:41:15 +0200

    simple_counter_in_react

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React from 'react';
 import Counter from "./Counter";
 
 function App() {
-  return (
-    <React.Fragment>
-      <Counter/>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <Counter/>
+        </React.Fragment>
+    );
 }
 
 export default App;
