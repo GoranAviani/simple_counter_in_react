0ec6f08a3ebaec98066bd8ec9abc93cbb7145856
GoranAviani <goranaviani@gmail.com>
2022-05-13 20:40:45 +0200

    simple_counter_in_react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Counter from "./Counter";
 
 function App() {
   return (
     <React.Fragment>
-      App
+      <Counter/>
     </React.Fragment>
   );
 }
